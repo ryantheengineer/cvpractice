33b5a023a32a7cbc034496bb5075aeed1a7832b2
ryantheengineer <ryan.larson.wa@gmail.com>
2020-05-01 23:31:57 -0600

    Finished copying exercise to write detect_faces.py

--- detect_faces.py
+++ detect_faces.py
@@ -41,4 +41,22 @@ for i in range(0, detections.shape[2]):
     # extract the confidence (i.e., probability) associated with the prediction
     confidence = detections[0, 0, i, 2]
 
-    
+    # filter out weak detections by ensuring the confidence is greater than the
+    # minimum confidence
+    if confidence > args["confidence"]:
+        # compute the (x, y)-coordinates of the bounding box for the object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        # draw the bounding box of the face along with the associated
+        # probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(image, (startX, startY), (endX, endY),
+            (0, 0, 255), 2)
+        cv2.putText(image, text, (startX, y),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)

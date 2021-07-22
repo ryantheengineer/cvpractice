0ecd0ee86a20c232074bd7855e3d6ff53f5c415b
ryantheengineer <ryan.larson.wa@gmail.com>
2021-07-22 13:27:10 -0600

    Added moon image and moon_phase_detector.py

---
+++ moon_phase_detector.py
@@ -0,0 +1,22 @@
+"""
+    A computer vision project to calculate the % moon phase in an image.
+"""
+
+# import the necessary packages
+import numpy as np
+import cv2
+import imutils
+
+# Import image
+
+
+# Convert image to black and white
+
+
+# Detect moon in image and create mask
+
+
+# Fit circle to the moon mask
+
+
+# Calculate the percentage of "lit" pixels that are within the circular area
